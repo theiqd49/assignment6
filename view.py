36674a2b1e537b96742c71bb5b3d566acfebf7d7
Zed <zedd.yuan@gmail.com>
2019-10-19 00:48:48 -0500

    conneted view.html to DB

--- view.py
+++ view.py
@@ -1,18 +1,26 @@
-from flask import Flask, redirect, url_for
-from flask import render_template
+from flask import Flask, redirect, url_for, render_template
+from theme_db_api import theme_db_api
 
 app = Flask(__name__)
+# access mongodb
+server = theme_db_api()
+theme = server.get_all_theme()
 
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=["GET", "POST"])
 def view():
-    return render_template(
-        'view.html'
-    )
+    global theme
+    # can obtain data if refreshing page
+    theme = list(server.get_all_theme())
+    if len(theme) == 0:
+        # when theme list is empty; 'length' is needed in html
+        return render_template('view.html', theme=theme, length=len(theme))
 
+    return render_template('view.html', theme=theme, length=len(theme))
 
-@app.route('/view_one/', methods=('GET', 'POST'))
-def view_one():
+# TODO should have a reports table with theme attribute
+@app.route('/view_one/<name>', methods=["GET", "POST"])
+def view_one(name):
     return render_template(
         "view_one.html"
     )
